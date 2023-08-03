7d1ff4a2fff4e61f9779ebfd059062183d5af72b
Old-Second <oldsecond2@qq.com>
2023-08-04 04:04:18 +0800

    refactor: 修改fetch

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,12 +1,12 @@
 const preUrl = '/api/v2';
 
-export async function post(url = '', data = {}, method: 'POST') {
+export async function post(url = '', data = {}) {
   const token = localStorage.getItem('token');
   if (!token) {
-    return;
+    throw new Error('未登录');
   }
-  const response = await fetch(preUrl + url, {
-    method,
+  const response = await fetch(`${preUrl}${url}`, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
       Authorization: token,
@@ -20,9 +20,9 @@ export async function post(url = '', data = {}, method: 'POST') {
 export async function get(url = '') {
   const token = localStorage.getItem('token');
   if (!token) {
-    return;
+    throw new Error('未登录');
   }
-  const response = await fetch(preUrl + url, {
+  const response = await fetch(`${preUrl}${url}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
@@ -34,13 +34,13 @@ export async function get(url = '') {
   return response.json();
 }
 
-export async function put(url = '', data = {}, method: 'PUT') {
+export async function put(url = '', data = {}) {
   const token = localStorage.getItem('token');
   if (!token) {
-    return;
+    throw new Error('未登录');
   }
-  const response = await fetch(preUrl + url, {
-    method,
+  const response = await fetch(`${preUrl}${url}`, {
+    method: 'PUT',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
       Authorization: token,
