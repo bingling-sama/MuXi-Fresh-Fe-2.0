c8b04007e57c194eb9353a5eac1018a3b6fbbb8f
Old-Second <oldsecond2@qq.com>
2023-08-10 15:17:59 +0800

    feat: 添加不需要token的fetch

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,50 +1,67 @@
+import { message } from 'antd';
+
 const preUrl = '/api/v2';
 
-export async function post(url = '', data = {}) {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    throw new Error('未登录');
+export async function post(url = '', data = {}, isToken?: boolean) {
+  const headers = new Headers({
+    'Content-Type': 'application/json;charset=utf-8',
+  });
+
+  if (isToken) {
+    const token = localStorage.getItem('token');
+    if (token) headers.append('Authorization', token);
+    else {
+      void message.error('未登录！');
+    }
   }
+
   const response = await fetch(`${preUrl}${url}`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: token,
-    },
+    headers,
     body: JSON.stringify(data),
   });
 
   return response.json();
 }
 
-export async function get(url = '') {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    throw new Error('未登录');
+export async function get(url = '', isToken?: boolean) {
+  const headers = new Headers({
+    'Content-Type': 'application/json;charset=utf-8',
+  });
+
+  if (isToken) {
+    const token = localStorage.getItem('token');
+    if (token) headers.append('Authorization', token);
+    else {
+      void message.error('未登录！');
+    }
   }
+
   const response = await fetch(`${preUrl}${url}`, {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: token,
-    },
+    headers,
     redirect: 'follow',
   });
 
   return response.json();
 }
 
-export async function put(url = '', data = {}) {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    throw new Error('未登录');
+export async function put(url = '', data = {}, isToken?: boolean) {
+  const headers = new Headers({
+    'Content-Type': 'application/json;charset=utf-8',
+  });
+
+  if (isToken) {
+    const token = localStorage.getItem('token');
+    if (token) headers.append('Authorization', token);
+    else {
+      void message.error('未登录！');
+    }
   }
+
   const response = await fetch(`${preUrl}${url}`, {
     method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: token,
-    },
+    headers,
     body: JSON.stringify(data),
   });
 
