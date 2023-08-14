d168d728c9e211efef0c7fba5131fa46e9632c11
Old-Second <oldsecond2@qq.com>
2023-08-15 01:32:59 +0800

    feat: 添加未登录或token过期自动跳转登录页面和退出登录功能

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout/Layout.tsx';
 import AuthorityManage from './pages/AuthorityManage/AuthorityManage.tsx';
 import Review from './pages/Review/Review.tsx';
+import Verify from './components/Verify/Verify.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <Verify element={<Layout />} />,
     children: [
       { path: '/review', element: <Review /> },
       {
