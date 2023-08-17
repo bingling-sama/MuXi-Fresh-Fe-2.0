7d9cc1e4414c9fe1210c03657a07733355628ea4
Old-Second <oldsecond2@qq.com>
2023-08-17 22:56:23 +0800

    feat: 添加Layout身份判断和作业部分

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -14,9 +14,17 @@ import {
   Menu,
   message,
 } from 'antd';
+import { UserIdentity, UserIsForm } from '../Verify/UserIdentity.ts';
+import { ItemType } from 'antd/es/menu/hooks/useItems';
 
 const { Header, Content, Sider } = LayoutAntd;
-const Layout: React.FC = () => {
+
+type LayoutProps = {
+  identity: UserIdentity;
+  isForm: UserIsForm;
+};
+
+const Layout: React.FC<LayoutProps> = ({ identity, isForm }) => {
   const [avatar, setAvatar] = useState(defaultAvatar);
 
   useEffect(() => {
@@ -62,6 +70,92 @@ const Layout: React.FC = () => {
     },
   ];
 
+  const menus: MenuProps['items'] = [
+    {
+      key: '/1',
+      title: '报名表',
+      label: <div className={'layoutLink'}>报名表</div>,
+    },
+    {
+      key: '/progress',
+      title: '进度查询',
+      label: <div className={'layoutLink'}>进度查询</div>,
+    },
+    isForm === '已交表'
+      ? {
+          key: '/homework',
+          title: '作业',
+          label: <div className={'layoutLinkTestTitle'}>作业</div>,
+          children:
+            identity === 'super_admin' || identity === 'admin'
+              ? [
+                  {
+                    key: '/homework/admin/new',
+                    title: '新作业',
+                    label: (
+                      <div className={'layoutLinkTest'} style={{ fontSize: '1vw' }}>
+                        新作业
+                      </div>
+                    ),
+                  },
+                  {
+                    key: '/homework/admin/edit',
+                    title: '修改作业',
+                    label: (
+                      <div className={'layoutLinkTest'} style={{ fontSize: '1vw' }}>
+                        修改作业
+                      </div>
+                    ),
+                  },
+                  {
+                    key: '/homework/admin/browse',
+                    title: '查看作业',
+                    label: (
+                      <div className={'layoutLinkTest'} style={{ fontSize: '1vw' }}>
+                        查看作业
+                      </div>
+                    ),
+                  },
+                ]
+              : [
+                  {
+                    key: '/homework/user/submit',
+                    title: '提交作业',
+                    label: (
+                      <div className={'layoutLinkTest'} style={{ fontSize: '1vw' }}>
+                        提交作业
+                      </div>
+                    ),
+                  },
+                ],
+        }
+      : {
+          key: '/homework/visitor',
+          title: '作业',
+          label: <div className={'layoutLink'}>作业</div>,
+        },
+    identity === 'super_admin' || identity === 'admin'
+      ? {
+          key: '/review',
+          title: '审阅',
+          label: <div className={'layoutLink'}>审阅</div>,
+        }
+      : ({} as ItemType),
+    identity === 'super_admin' || identity === 'admin'
+      ? {
+          key: '/authority-manage',
+          title: '权限管理',
+          label: <div className={'layoutLink'}>权限管理</div>,
+        }
+      : ({} as ItemType),
+    identity === 'super_admin' || identity === 'admin'
+      ? ({} as ItemType)
+      : {
+          key: '/9',
+          title: '入职测验',
+          label: <div className={'layoutLink'}>入职测验</div>,
+        },
+  ];
   return (
     <div className={'layout'}>
       <Header
@@ -104,38 +198,12 @@ const Layout: React.FC = () => {
             <Menu
               mode="inline"
               selectedKeys={[location.pathname]}
+              items={menus}
               onSelect={(e) => {
                 navigationClick(e.key);
               }}
               style={{ minWidth: 0, flex: 'auto' }}
-            >
-              <Menu.Item key="/1" className={'layoutLink'}>
-                报名表
-              </Menu.Item>
-              <Menu.Item key="/progress" className={'layoutLink'}>
-                进度查询
-              </Menu.Item>
-              <Menu.SubMenu key="/3" className={'layoutLinkTestTitle'} title={'作业'}>
-                <Menu.Item key="/4" className={'layoutLinkTest'}>
-                  新作业
-                </Menu.Item>
-                <Menu.Item key="/5" className={'layoutLinkTest'}>
-                  修改作业
-                </Menu.Item>
-                <Menu.Item key="/6" className={'layoutLinkTest'}>
-                  查看作业
-                </Menu.Item>
-              </Menu.SubMenu>
-              <Menu.Item key="/review" className={'layoutLink'}>
-                审阅
-              </Menu.Item>
-              <Menu.Item key="/authority-manage" className={'layoutLink'}>
-                权限管理
-              </Menu.Item>
-              <Menu.Item key="/9" className={'layoutLink'}>
-                入职测验
-              </Menu.Item>
-            </Menu>
+            />
           </ConfigProvider>
         </div>
       </Sider>
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Layout from './components/Layout/Layout.tsx';
 import AuthorityManage from './pages/AuthorityManage/AuthorityManage.tsx';
 import Review from './pages/Review/Review.tsx';
 import SignIn from './pages/SignIn/SignIn.tsx';
@@ -7,14 +6,66 @@ import SignUp from './pages/SignUp/SignUp.tsx';
 import HomePage from './pages/HomePage/HomePage.tsx';
 import Progress from './pages/Progress/Progress.tsx';
 import Verify from './components/Verify/Verify.tsx';
+import HomeWork from './pages/homework/pages';
+import HomeworkVisitorMode from './pages/homework/pages/visitorMode';
+import HomeworkAdminMode from './pages/homework/pages/adminMode';
+import HomeworkNew from './pages/homework/pages/adminMode/new';
+import HomeworkEdit from './pages/homework/pages/adminMode/edit';
+import HomeworkJudge from './pages/homework/pages/adminMode/judge';
+import HomeworkBrowse from './pages/homework/pages/adminMode/browse';
+import HomeworkUserMode from './pages/homework/pages/userMode';
+import HomeworkUserSubmit from './pages/homework/pages/userMode/submit';
 
 const router = createBrowserRouter([
+  { path: '/login', element: <SignIn /> },
+  { path: '/signUp', element: <SignUp /> },
   {
     path: '/',
-    element: <Verify element={<Layout />} />,
+    element: <Verify />,
     children: [
       { path: '/home', element: <HomePage /> },
       { path: '/progress', element: <Progress /> },
+      {
+        // 作业
+        path: '/homework',
+        element: <HomeWork></HomeWork>,
+        children: [
+          // { path: '/', element: <HomeworkVisitorMode></HomeworkVisitorMode> },
+          {
+            // 管理员
+            path: 'admin',
+            element: <HomeworkAdminMode></HomeworkAdminMode>,
+            children: [
+              { path: 'new', element: <HomeworkNew></HomeworkNew> },
+              {
+                path: 'edit',
+                element: <HomeworkEdit></HomeworkEdit>,
+              },
+              { path: 'judge', element: <HomeworkJudge></HomeworkJudge> },
+              {
+                path: 'browse',
+                element: <HomeworkBrowse></HomeworkBrowse>,
+              },
+            ],
+          },
+          {
+            // 用户
+            path: 'user',
+            element: <HomeworkUserMode></HomeworkUserMode>,
+            children: [
+              {
+                path: 'submit',
+                element: <HomeworkUserSubmit></HomeworkUserSubmit>,
+              },
+            ],
+          },
+          {
+            // 游客
+            path: 'visitor',
+            element: <HomeworkVisitorMode></HomeworkVisitorMode>,
+          },
+        ],
+      },
       { path: '/review', element: <Review /> },
       {
         path: '/authority-manage',
@@ -22,8 +73,6 @@ const router = createBrowserRouter([
       },
     ],
   },
-  { path: '/login', element: <SignIn /> },
-  { path: '/signUp', element: <SignUp /> },
 ]);
 
 export default router;
