08f49fc8e6cf8cbabf9fe109af56218e7e44a59e
Old-Second <oldsecond2@qq.com>
2023-08-21 00:10:50 +0800

    feat: 添加pc和移动端切换路由

--- src/router.tsx
+++ src/router.tsx
@@ -2,19 +2,29 @@ import { createBrowserRouter } from 'react-router-dom';
 import AuthorityManage from './pages/AuthorityManage/AuthorityManage.tsx';
 import Review from './pages/Review/Review.tsx';
 import Verify from './components/Verify/Verify.tsx';
+import { isDesktop } from 'react-device-detect';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Verify />,
-    children: [
-      { path: '/review', element: <Review /> },
-      {
-        path: '/authority-manage',
-        element: <AuthorityManage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  isDesktop
+    ? [
+        {
+          path: '/',
+          element: <Verify />,
+          children: [
+            { path: '/review', element: <Review /> },
+            {
+              path: '/authority-manage',
+              element: <AuthorityManage />,
+            },
+          ],
+        },
+      ]
+    : [
+        {
+          path: '/',
+          element: <Verify />,
+        },
+      ],
+);
 
 export default router;
