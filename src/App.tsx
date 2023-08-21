89fa136e01489f127832b085f5fe56c783a0b6ae
poememory <3269187297@qq.com>
2023-08-21 12:22:11 +0800

    Merge branch 'wns' of https://github.com/Muxi-X/MuXi-Fresh-Fe-2.0 into wns

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import FormForWeb from './pages/formW';
 function App() {
   localStorage.setItem(
     'token',
+    //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTI5MTI4OTAsImlhdCI6MTY5MjU1Mjg5MCwiand0VXNlcklkIjoiNjRlMjRlYmFhYWE5OTJmZWQxMDc3ZjQxIn0.so3pKrQteBBWUaEh2Qoyfx42qCucsDvApNRYCk-qohg',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTI2ODM5OTAsImlhdCI6MTY5MjMyMzk5MCwiand0VXNlcklkIjoiNjRjOTI2MjVjMDBkYjE4ODdhZjM4Yzg4In0.lnCSqHlZY7otqvjch5-MVmRWo-QluZ8Uk8dzzzCPXCs',
   );
 
