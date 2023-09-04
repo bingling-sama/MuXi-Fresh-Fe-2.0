478722ee0b80fe55172c36f4721aa785ade542e6
konokono <1845519693@qq.com>
2023-09-04 16:46:38 +0800

    fix: 增加作业路由权限

--- src/pages/homework/pages/adminMode/index.tsx
+++ src/pages/homework/pages/adminMode/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { get } from '../../../../fetch.ts';
+import { backType, formStatusType } from '../../types';
+import { message } from 'antd';
 
 const HomeworkAdminMode: React.FC = () => {
+  const nav = useNavigate();
+  useEffect(() => {
+    get('/form/judge').then((res: backType<formStatusType>) => {
+      console.log(res.data.user_type === 'admin');
+      if (res.data.user_type !== 'admin') {
+        message.error('无权限访问').then(null, null);
+        nav('/app/homework/user/submit');
+      }
+    }, null);
+  }, [nav]);
   return (
     <div
       style={{
